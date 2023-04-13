a449d97619807d2078cc3be5b6a1d2d41890a758
Grant Hough <grant.l.hough@gmail.com>
2023-04-13 01:23:24 -0700

    able to generate a massive array with training times and accuracies per epoch based on loss function AND learning rate

--- WWDC2023.swiftpm/Package.swift
+++ WWDC2023.swiftpm/Package.swift
@@ -19,7 +19,7 @@ let package = Package(
             bundleIdentifier: "granthough.WWDC2023",
             displayVersion: "1.0",
             bundleVersion: "1",
-            appIcon: .placeholder(icon: .pencil),
+            appIcon: .asset("AppIcon"),
             accentColor: .presetColor(.purple),
             supportedDeviceFamilies: [
                 .pad,
