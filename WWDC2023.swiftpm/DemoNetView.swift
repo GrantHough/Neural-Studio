ef72c81839ad6417c3a74d16264b22863878dca6
Grant Hough <grant.l.hough@gmail.com>
2023-04-14 22:24:56 -0700

    more good work

---
+++ WWDC2023.swiftpm/DemoNetView.swift
@@ -0,0 +1,20 @@
+//
+//  DemoNetView.swift
+//  WWDC2023
+//
+//  Created by Grant Hough on 4/14/23.
+//
+
+import SwiftUI
+
+struct DemoNetView: View {
+    var body: some View {
+        Text("Demo of net with good combo of stuff")
+    }
+}
+
+struct DemoNetView_Previews: PreviewProvider {
+    static var previews: some View {
+        DemoNetView()
+    }
+}
--- WWDC2023.swiftpm/MyApp.swift
+++ WWDC2023.swiftpm/MyApp.swift
@@ -8,3 +8,20 @@ struct MyApp: App {
         }
     }
 }
+
+//extension to double class to add functions so that I can truncate float values
+
+extension Double {
+    func round(_ places:Int) -> Double {
+        let d = pow(10.0, Double(places))
+        return (self * d).rounded() / d
+    }
+    func truncate(_ places:Int) -> Double {
+        let d = pow(10.0, Double(places))
+        return (self*d).rounded(.towardZero) / d
+    }
+}
+
+func percentToString(percent: Double) -> String {
+    return "\(String(percent))%"
+}
